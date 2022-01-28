595032762f3f012d8407686d62890bcdb3682205
gkysaad <g.saad@mail.utoronto.ca>
2022-01-27 23:52:30 -0500

    set up database connection

--- app/main.py
+++ app/main.py
@@ -1,5 +1,11 @@
+import os
 from flask import Flask
-app= Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+db = SQLAlchemy(app)
+
 @app.route('/')
 def index():
   return "<h1>Hello World!</h1>"
