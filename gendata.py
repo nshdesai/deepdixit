e3f8f31357bb9f034822ddb122f61b728758c0e5
Nishkrit Desai <nishkritdesai10@gmail.com>
2022-02-03 00:42:43 -0500

    Added deep-daze data generation functionality

--- gendata.py
+++ gendata.py
@@ -6,14 +6,19 @@ import os
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
-from big_sleep import Imagine
+from big_sleep import Imagine as bsImagine
+from deep_daze import Imagine as ddImagine
 import signal
 from shutil import rmtree
 
-@click.command()
+@click.group()
+def generate():
+    pass
+
+@generate.command()
 @click.argument('prompt_file', type=click.Path(exists=True, resolve_path=True, path_type=Path))
 @click.argument('output_dir', type=click.Path(resolve_path=True, path_type=Path))
-def main(prompt_file, output_dir):
+def deepdaze(prompt_file, output_dir):
     """
     Generate data for the prompt-response task.
     """
@@ -34,7 +39,50 @@ def main(prompt_file, output_dir):
 
     df = pd.read_csv(prompt_file)
 
-    imagine = Imagine(
+    for i, (prompt, cat) in tqdm(df.iterrows(), total=df.shape[0]):
+        subdir = output_dir / cat
+        subdir.mkdir(parents=True, exist_ok=True)
+        os.chdir(subdir)
+   
+        imagine = ddImagine(
+            text=prompt.lower() + "|" + cat.lower(),
+            open_folder=False,
+            gradient_accumulate_every=4,
+            epochs=2,
+            save_progress=False,
+            num_layers=42,
+            batch_size=64,
+            iterations=100
+        )
+        imagine()
+
+        os.chdir(output_dir)
+
+@generate.command()
+@click.argument('prompt_file', type=click.Path(exists=True, resolve_path=True, path_type=Path))
+@click.argument('output_dir', type=click.Path(resolve_path=True, path_type=Path))
+def bigsleep(prompt_file, output_dir):
+    """
+    Generate data for the prompt-response task.
+    """
+    print("Generating image/prompt data")
+    print("-"*50)
+    if output_dir.exists():
+        overwrite=click.confirm("Output directory already exists. Erase and Overwrite?", default=False)
+        if not overwrite:
+            print("Exiting.")
+            exit(1)
+        else:
+            print(f"Erasing and overwriting {output_dir}.")
+            rmtree(output_dir)
+    
+    output_dir.mkdir(parents=True, exist_ok=True)
+    os.chdir(output_dir)
+    print(f'Generating data in {output_dir}...')
+
+    df = pd.read_csv(prompt_file)
+
+    imagine = bsImagine(
         text_min="blur|zoom",
         open_folder=False,
         larger_clip=False,
@@ -55,4 +103,4 @@ def main(prompt_file, output_dir):
         os.chdir(output_dir)
 
 if __name__ == '__main__':
-    main()
+    generate()
