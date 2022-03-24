5ef2e4db657109e2819e349eb21262d1a31b8c71
gkysaad <g.saad@mail.utoronto.ca>
2022-03-24 00:37:31 -0400

    fix requirements.txt

--- app/main.py
+++ app/main.py
@@ -12,8 +12,8 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 app = Flask(__name__)
 CORS(app)
-print(f'DEBUG: {os.environ.get("DATABASE_URL")}')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+print(f'DEBUG: {os.environ.get("DATABASE_URI")}')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
