ae829ebb773c25675cdf86ba61349ec70c660a6c
gkysaad <g.saad@mail.utoronto.ca>
2022-03-23 21:20:10 -0400

    work on frotnend
    Co-authored-by: nshdesai <nshdesai@users.noreply.github.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Home from './Home';
 import Game from './Game';
 import About from './About';
+import Results from './Results';
 import './index.css';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       {page === 'home' && <Home pageSetter={setPage}/>}
       {page === 'game' && <Game pageSetter={setPage}/>}
       {page === 'about' && <About pageSetter={setPage}/>}
+      {page === 'results' && <Results pageSetter={setPage}/>}
     </div>
 
   );
--- frontend/src/Game.js
+++ frontend/src/Game.js
@@ -5,8 +5,15 @@ function Game({ pageSetter }) {
         <div className='wrapper'>
             <div className="nav-buttons">
                 <button className="nav-button" onClick={() => { pageSetter('home') }}>Home</button>
+                <button className="nav-button" onClick={() => { pageSetter('results') }}>Results</button>
             </div>
-            <h1>Game</h1>
+            <img src="https://media.giphy.com/media/3o7btLnjQXZqQQqZ3S/giphy.gif" alt="" />
+            <div className="multiple-choice">
+                <button>Choice 1</button>
+                <button>Choice 2</button>
+                <button>Choice 3</button>
+                <button>Choice 4</button>
+            </div>
         </div>
 
     );
