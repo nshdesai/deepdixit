19e1c1f6a9c0789c8c28d87721c3ae95915c50b8
gkysaad <g.saad@mail.utoronto.ca>
2022-04-07 18:52:42 -0400

    fix frontend

--- frontend/src/Presentation.js
+++ frontend/src/Presentation.js
@@ -43,46 +43,49 @@ function Presentation({ pageSetter }) {
                 <button className="nav-button" onClick={() => { pageSetter('results') }}>Results</button>
                 <button className="nav-button" onClick={() => { pageSetter('game') }}>Normal</button>
             </div>
-            <div className='score-presentation max-w-2xl rounded-2xl overflow-hidden shadow-2xl bg-stone-50 p-5 mt-20'>
-                {score}/{rounds}
+            <div className='grid-container'>
+                <div className='score-presentation max-w-2xl rounded-2xl overflow-hidden shadow-2xl bg-stone-50 p-5 mt-20'>
+                    {score}/{rounds}
+                </div>
+                <div class="max-w-2xl rounded-2xl overflow-hidden shadow-2xl bg-orange-400 p-10 mt-20 img-card-presentation">
+                    <img src={`data:image/png;base64, ${randImage.image}`} className="rounded-2xl" />
+                </div>
+                <div className="multiple-choice-presentation">
+                    {randImage.prompts.map((prompt, index) => {
+                        return (
+                            <button className={`choice ${btnColors[index]} ` + `${choiceSelected != -1 ? (index === choiceSelected ? 'text-white ' : '') : 'hover:bg-amber-50'} rounded-full shadow-xl p-2 text-center text-4xl w-80 h-24`} onClick={() => {
+                                if (prompt.real === true && choiceSelected === -1) {
+                                    let newBtnColors = btnColors.slice();
+                                    newBtnColors[index] = 'bg-emerald-400';
+                                    setBtnColors(newBtnColors);
+                                    setChoiceSelected(index);
+                                    setScore(score + 1);
+                                    setRounds(rounds + 1);
+                                } else if (prompt.real === false && choiceSelected === -1) {
+                                    let newBtnColors = btnColors.slice();
+                                    newBtnColors[index] = 'bg-red-500';
+                                    newBtnColors[randImage.prompts.map((p) => { return p.real === true }).indexOf(true)] = 'bg-emerald-400';
+                                    setBtnColors(newBtnColors);
+                                    setChoiceSelected(index);
+                                    setRounds(rounds + 1);
+                                }
+                            }}>{prompt.prompt}</button>
+                        )
+                    }, this)}
+                    {choiceSelected !== -1 && (
+                        <button className="bg-orange-500 hover:bg-orange-600 text-white rounded-full p-4 font-title text-4xl mb-12 h-24 next-btn-presentation" onClick={() => {
+                            let newBtnColors = btnColors.slice();
+                            newBtnColors.fill('bg-amber-100');
+                            setBtnColors(newBtnColors);
+                            setChoiceSelected(-1);
+                            setNexts(nexts + 1);
+                        }}>
+                            Next Round
+                        </button>
+                    )}
+                </div>
+
             </div>
-            <div class="max-w-2xl rounded-2xl overflow-hidden shadow-2xl bg-orange-400 p-10 mt-20 img-card-presentation">
-                <img src={`data:image/png;base64, ${randImage.image}`} className="rounded-2xl" />
-            </div>
-            <div className="multiple-choice-presentation">
-                {randImage.prompts.map((prompt, index) => {
-                    return (
-                        <button className={`choice ${btnColors[index]} ` + `${choiceSelected != -1 ? (index === choiceSelected ? 'text-white ' : '') : 'hover:bg-amber-50'} rounded-full shadow-xl p-2 text-center text-4xl w-80`} onClick={() => {
-                            if (prompt.real === true && choiceSelected === -1) {
-                                let newBtnColors = btnColors.slice();
-                                newBtnColors[index] = 'bg-emerald-400';
-                                setBtnColors(newBtnColors);
-                                setChoiceSelected(index);
-                                setScore(score + 1);
-                                setRounds(rounds + 1);
-                            } else if (prompt.real === false && choiceSelected === -1) {
-                                let newBtnColors = btnColors.slice();
-                                newBtnColors[index] = 'bg-red-500';
-                                newBtnColors[randImage.prompts.map((p) => { return p.real === true }).indexOf(true)] = 'bg-emerald-400';
-                                setBtnColors(newBtnColors);
-                                setChoiceSelected(index);
-                                setRounds(rounds + 1);
-                            }
-                        }}>{prompt.prompt}</button>
-                    )
-                }, this)}
-            </div>
-            {choiceSelected !== -1 && (
-                <button className="bg-orange-500 hover:bg-orange-600 text-white rounded-full p-4 font-title text-4xl mb-12 next-btn" onClick={() => {
-                    let newBtnColors = btnColors.slice();
-                    newBtnColors.fill('bg-amber-100');
-                    setBtnColors(newBtnColors);
-                    setChoiceSelected(-1);
-                    setNexts(nexts + 1);
-                }}>
-                    Next Round
-                </button>
-            )}
         </div>
 
     );
